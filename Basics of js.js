f04f096d3bfb173c33077111e66eeed0c0b96c9f
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-03-17 18:05:11 +0530

    Create Basics of js.js

---
+++ Basics of js.js
@@ -0,0 +1,37 @@
+// Data types, variables, Operators, Conditions, Loops and Arrays and ...???
+// Data Type represents the type of the data
+// Numbers, String, Boolean
+// Boolean : true , false
+var name = "john doe"
+var age = 23;
+// Operator:
+// Arithemetic: +,-,*,/,%
+// ASSIGNEMNT: =
+// lOGICAL : and, or,not
+// Bitwise: &&, ||,!,^
+// Comparision: >,<,>=,<=,!=
+var a = 32;
+var b = 33;
+// comaprision operator always gives the Output in terms of true or false
+// If the condition is yes then it will give true
+// If the condition is no then it will give false
+// Conditional Statement:
+// if, if-else, if-else-if,else
+// Syntax: if(condition){}else{}
+if(a<b){
+console.log("a is greater than b");
+}
+else{
+console.log("a is lesser than b");
+}
+
+// Loop:
+// A iterative Statement
+// for loop:
+// for loop is a fixed execution looping statements
+
+
+// Write a simple program to print the Multiples of 12
+for(var i =1;i<=10;i++){
+console.log(12*i);
+}
