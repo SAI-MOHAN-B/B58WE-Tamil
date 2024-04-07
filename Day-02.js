2bee8170f14e38cf9977675201a80cf2c6c29dfc
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-04-07 17:53:05 +0530

    Added the Object properties

--- Day-02.js
+++ Day-02.js
@@ -22,3 +22,39 @@ console.log(arr.push(567));
 // Syntax: arrayname[indexnum]=element
 arr[2] = "guvi"
 console.log(arr);
+
+
+// Objects:
+// An Objects is a key:value pair Structure
+// Syntax: var objectname = {key:value}
+
+
+// Ex:1 Details of the Student
+var stuname = {
+  name:"john doe",
+  age:23,
+  year:2018
+}
+// Accessing:
+// 2 types of Accessing are there
+
+// 1. Method (.)DOT method (commonly used)
+// 2. Box Method
+
+
+//1 DOT method
+// Syntax: objectname.keyname
+console.log(stuname.name)
+
+// Create a new key:value pair...
+// Syntax:objectname.keyname = value
+stuname.gender = "male"
+console.log(stuname)
+// Update
+// Syntax:objectname.keyname = value
+stuname.name = "mary"
+
+// delete
+// Syntax: delete objectname.keyname
+delete stuname.age
+console.log(stuname)
